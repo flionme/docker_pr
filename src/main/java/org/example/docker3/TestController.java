3b3b85f2ac07bc5dd65ec1e98fe70f842278dd34
flionme <sunginkim142@gmail.com>
2025-09-10 10:57:37 +0900

    dd

--- src/main/java/org/example/docker3/TestController.java
+++ src/main/java/org/example/docker3/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test(){
-        System.out.println("test");
         return ResponseEntity.status(200).body("Test code");
     }
 }
