2c887711698a5160a85291118313a875188f27cd
flionme <sunginkim142@gmail.com>
2025-09-10 10:09:05 +0900

    dd

---
+++ src/main/java/org/example/docker3/TestController.java
@@ -0,0 +1,14 @@
+package org.example.docker3;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity test(){
+
+        return ResponseEntity.status(200).body("Test code");
+    }
+}
