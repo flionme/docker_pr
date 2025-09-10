1ff2df08136bc120d744252648b15e13b53753d9
flionme <sunginkim142@gmail.com>
2025-09-10 15:41:33 +0900

    dd

--- src/main/java/org/example/docker3/TestController.java
+++ src/main/java/org/example/docker3/TestController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController("/api")
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity test(){
